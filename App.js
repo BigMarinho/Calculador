cfe638a2ebbc47d8f5973b4af4a5be96e043f04a
Gabriel Marinho <eltarukbr@gmail.com>
2023-04-08 16:27:53 -0300

    Alteração nas cores dos botões

--- App.js
+++ App.js
@@ -76,12 +76,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#1e1240'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? '#fff': '#fff'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#1e1240"
   },
   resultText: {
-    color: "#282F38",
+    color: "#fff",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
@@ -118,7 +118,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#3d0075',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
